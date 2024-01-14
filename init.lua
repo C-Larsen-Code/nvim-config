3283cf51f5950f6aa2064e3b5e57619271fbcda1
Christian Larsen <larsen.christian.coding@gmail.com>
2024-01-13 17:49:52 -0700

    Added clang lsp capabilities

--- init.lua
+++ init.lua
@@ -487,8 +487,10 @@ require('mason-lspconfig').setup()
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  clangd = {},
-  -- gopls = {},
+  clangd = {
+    filetypes = { "c", "cpp" }
+  },
+  gopls = {},
   -- pyright = {},
   rust_analyzer = {},
   -- tsserver = {},
